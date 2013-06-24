50926048671f234b08329903855896abe930ec51
Hack Reactor Students <github@hackreactor.com>
2013-06-24 16:36:38 -0700

    Fat dancer FOR REAL

---
+++ src/fatDancer.js
@@ -0,0 +1,20 @@
+var FatDancer = function(top, left, timeBetweenSteps){
+  Dancer.apply(this, arguments);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+FatDancer.prototype = new Dancer();
+FatDancer.prototype.constructor = FatDancer;
+oldStep = Dancer.prototype.step;
+
+FatDancer.prototype.step = function(){
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep.call(this);
+
+    /* toggle() is a jQuery method to show/hide the <span> tag.
+     * See http://api.jquery.com/category/effects/ for this and
+     * other effects you can use on a jQuery-wrapped html tag. */
+    this.$node.toggleClass('fat');
+    this.$node.toggleClass('fat2');
+  };
