6127d173c9c78d99fe6762519cf1ee16e14866ec
Hack Reactor Students <github@hackreactor.com>
2013-06-24 13:18:52 -0700

    Refactored functional inheritance pattern to pseudoclassical.

--- src/dancer.js
+++ src/dancer.js
@@ -1,18 +1,27 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
-
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
-  console.log("Initial def " + this.$node)
   this.$node = $('<span class="dancer"></span>');
 
-  this.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, timeBetweenSteps);
-  };
   this.step();
 
-  this.setPosition = function(top, left){
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+
+};
+
+makeDancer.prototype.step = function(){
+    // the basic dancer doesn't do anything interesting at all on each step,
+    // it just schedules the next step
+    setTimeout(this.step, this.timeBetweenSteps);
+  };
+
+makeDancer.prototype.setPosition = function(top, left){
     /* Use css top and left properties to position our <span> tag
      * where it belongs on the page. See http://api.jquery.com/css/
      */
@@ -20,12 +29,5 @@ var makeDancer = function(top, left, timeBetweenSteps){
       top: top,
       left: left
     };
-    console.log("set Position" + this.$node)
     this.$node.css(styleSettings);
   };
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
-
-};
