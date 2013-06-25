0f880740a6460587017301f9c056c97118a40d16
Hack Reactor Students <github@hackreactor.com>
2013-06-25 15:46:19 -0700

    Following dancer now has a swarm like effect.

--- src/followingDancer.js
+++ src/followingDancer.js
@@ -2,6 +2,8 @@ var FollowingDancer = function(top, left, timeBetweenSteps){
   Dancer.apply(this, arguments);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  this.randomFactor = Math.random() * 20;
+  this.$node.data('ran', this.randomFactor);
 };
 
 FollowingDancer.prototype = Object.create(Dancer.prototype);
